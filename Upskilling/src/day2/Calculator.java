d0084721b848056858214859342cf71a2463710f
imranilyas <68575807+imranilyas@users.noreply.github.com>
2022-01-05 09:56:12 -0800

    Create Calculator.java

---
+++ Upskilling/src/day2/Calculator.java
@@ -0,0 +1,18 @@
+package day2;
+
+public class Calculator {
+	public int num1;
+	public int num2;
+	
+	public int add() {
+		return num1 + num2;
+	}
+	
+	public int subtract() {
+		return num1 - num2;
+	}
+	
+	public int multiply() {
+		return num1 * num2;
+	}
+}
